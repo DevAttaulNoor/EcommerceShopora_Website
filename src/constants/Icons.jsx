586909ceb9913a5bfcce4b0ee037d3bf9040b746
Atta ul Noor <attaulnoor2001@gmail.com>
2025-12-16 18:17:18 +0500

    Updated

---
+++ src/constants/Icons.jsx
@@ -0,0 +1,8 @@
+import { MdFavoriteBorder } from "react-icons/md";
+import { FaUser, FaCartShopping } from "react-icons/fa6";
+
+export const Icons = {
+    USER: <FaUser />,
+    CART: <FaCartShopping />,
+    FAVORITE_HEART: <MdFavoriteBorder />,
+}
